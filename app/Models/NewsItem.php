7f15acbd5bc815ff2480f24601b2df1c2ece94d6
zaka-krc <zakariakorchi2003@hotmail.com>
2025-05-21 00:01:46 +0200

    Nieuwsmodule: basisstructuur opgezet

---
+++ app/Models/NewsItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NewsItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'image',
+        'content',
+        'publication_date',
+        'user_id',
+    ];
+
+    // Relatie met user (admin die het nieuws heeft aangemaakt)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
